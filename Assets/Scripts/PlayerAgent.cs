5cf1d32b94a3df5682719a8e3ab2ea56ceeb843f
Dilmer Valecillos <dilmervst@gmail.com>
2019-11-26 17:46:19 -0700

    Initial project files

---
+++ Assets/Scripts/PlayerAgent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAgent : MonoBehaviour
+{
+    [SerializeField]
+    private Transform goal;
+
+    void Start()
+    {
+        var playerAgent = GetComponent<NavMeshAgent>();
+        playerAgent.destination = goal.position;
+    }
+}
