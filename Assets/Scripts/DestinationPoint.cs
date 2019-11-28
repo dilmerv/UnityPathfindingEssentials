d92a73808fa7043fc3253ad4d70c935028e94ed9
Dilmer Valecillos <dilmervst@gmail.com>
2019-11-28 14:32:09 -0700

    Added part 4

---
+++ Assets/Scripts/DestinationPoint.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DestinationPoint : MonoBehaviour
+{
+    [SerializeField]
+    private float force = 10.0f;
+
+    [SerializeField]
+    private TextMeshPro overlay;
+
+    private string originalText;
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if(other.gameObject.name == "Player")
+        {
+            originalText = overlay.text;
+            overlay.text = "ACTIVATED";
+            other.gameObject.GetComponent<Rigidbody>().AddForce(force * Vector3.up, ForceMode.Force);
+        }
+    }
+
+    private void OnCollisionExit(Collision other) 
+    {
+        if(other.gameObject.name == "Player")
+        {
+            overlay.text = originalText;
+        }
+    }
+}
